98f7353927c928cdb9e7954280720bf138e90ece
powerspt <45279624+powerspt@users.noreply.github.com>
2024-10-02 09:19:44 +0900

    day4

--- 15025.py
+++ 15025.py
@@ -1,2 +1,15 @@
 #15025.py
 
+a, b = map(int, input().split())
+
+count = a + b
+
+if a == 0 and b == 0:
+    print('Not a moose')
+elif a == b:
+   print('Even', a+b)
+else:
+    if a > b:
+        print('Odd', a*2)
+    else:
+        print('Odd', b*2)
